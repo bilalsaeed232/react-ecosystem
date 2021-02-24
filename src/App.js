40d2a2148acf8a93222d3b746a01e175376a0000
Bilal Saeed <bilalsaeed232@gmail.com>
2021-02-24 19:28:10 +0100

    ADD: hot reload tool for live reload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { hot } from "react-hot-loader";
 
 import './App.css';
 
@@ -6,7 +7,8 @@ import './App.css';
 const App = () => (
     <div className="App">
         <h1>Hello, World!!</h1>
+        <p>A quick brown fox jumps over the lazy dog.</p>
     </div>
 );
 
-export default App;
+export default hot(module)(App);
