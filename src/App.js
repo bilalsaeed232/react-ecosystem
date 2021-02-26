2001945bca58c169171fa2cddc87bb735b82ac38
Bilal Saeed <bilalsaeed232@gmail.com>
2021-02-26 19:18:28 +0100

    install styled-component, change App component with styled-component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { hot } from "react-hot-loader";
+import styled from "styled-components";
+
 import TodoList from './todos/TodoList';
 
-import './App.css';
+const AppContainer = styled.div`
+    margin: 1rem;
+    font-family: Arial, Helvetica, sans-serif;
+    color: #555555;
+`
+
 
 
 const App = () => (
-    <div className="App">
+    <AppContainer>
         <h1>Todo Application</h1>
         <TodoList />
-    </div>
+    </AppContainer>
 );
 
 export default hot(module)(App);
