a209f0e3365482bc989cb02c5c8eea22167c7ee5
Bilal Saeed <bilalsaeed232@gmail.com>
2021-02-26 15:09:25 +0100

    ADD: loadTodos thunk to get todos from api

--- src/todos/actions.js
+++ src/todos/actions.js
@@ -15,3 +15,22 @@ export const markTodo = (text) => ({
     type: MARK_TODO,
     payload: {text}
 })
+
+
+export const LOAD_TODOS_IN_PROGRESS = 'LOAD_TODOS_IN_PROGRESS';
+export const loadTodosInProgress = () => ({
+    type: LOAD_TODOS_IN_PROGRESS
+});
+
+export const LOAD_TODOS_SUCCESS = 'LOAD_TODOS_SUCCESS';
+export const loadTodosSuccess = (todos) => ({
+    type: LOAD_TODOS_SUCCESS,
+    payload: { todos }
+});
+
+export const LOAD_TODOS_FAILURE = 'LOAD_TODOS_FAILURE';
+export const loadTodosFailure = () => ({
+    type: LOAD_TODOS_FAILURE
+});
+
+
--- src/todos/thunks.js
+++ src/todos/thunks.js
@@ -1,3 +1,20 @@
+import { loadTodosInProgress, loadTodosSuccess, loadTodosFailure } from "./actions";
+
 export const displayAlert = (e) => () => {
     alert(e);
+}
+
+export const loadTodos = () => async (dispatch, getState) => {
+    //api call here
+    try {    
+        dispatch(loadTodosInProgress());
+        
+        const response = await fetch('http://localhost:8080/todos');
+        const todos = await response.json();
+        
+        dispatch(loadTodosSuccess(todos));
+    } catch(e) {
+        dispatch(loadTodosFailure(e));
+        dispatch(displayAlert(e)); //for now just an alert is enough
+    }
 }
