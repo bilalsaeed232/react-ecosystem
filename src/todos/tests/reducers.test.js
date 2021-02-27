992678ed552d505e3b73acd3b357b2e4952c2b79
Bilal Saeed <bilalsaeed232@gmail.com>
2021-02-27 16:56:34 +0100

    ADD: more test cases for reducers

--- src/todos/tests/reducers.test.js
+++ src/todos/tests/reducers.test.js
@@ -3,7 +3,7 @@ import { todos } from "../reducers";
 
 
 describe("The Todos Reducer", ()=> {
-    it("Adds a new Todo with CREATE_TODO action is received", () => {
+    it("Adds a new Todo when CREATE_TODO action is received", () => {
         const fakeTodo = { text: "New Todo", isCompleted: false };
         const fakeAction = { 
             type: 'CREATE_TODO',
@@ -23,5 +23,59 @@ describe("The Todos Reducer", ()=> {
         const actual = todos(initialState, fakeAction);
 
         expect(actual).to.deep.equal(expected);
-    })
+    });
+
+    it("Removes the Todo when REMOVE_TODO action is received", () => {
+        const fakeTodo = {
+            id: "232",
+            text: "I am fake",
+            isCompleted: false
+        }
+        const fakeAction = {
+            type: 'REMOVE_TODO',
+            payload: {todo: fakeTodo}
+        }
+
+        const initialState = {
+            isLoading: false,
+            data: [{id: "123", text: "some other todo", isCompleted: false}, fakeTodo]
+        }
+
+        const expected = {
+            isLoading: false,
+            data: [{id: "123", text: "some other todo", isCompleted: false}]
+        }
+
+        const actual = todos(initialState, fakeAction);
+
+        expect(actual).to.deep.equal(expected);
+    });
+
+    it("Updates the Todo when UPDATE_TODO action is received", () => {
+        const fakeTodo = {id:"232", text:"I am a fake todo", isCompleted: false};
+        const fakeTodoToUpdate = {
+            id: "232",
+            text: "I am the updated todo",
+            isCompleted: false
+        }
+
+        const fakeAction = {
+            type: 'UPDATE_TODO', 
+            payload: { todo: fakeTodoToUpdate}
+        }
+
+        const initialState = {
+            isLoading: false,
+            data: [ fakeTodo ]
+        }
+
+        const expected = {
+            isLoading: false,
+            data: [ fakeTodoToUpdate ]
+        }
+
+        const actual = todos(initialState, fakeAction);
+
+        expect(actual).to.deep.equal(expected);
+    });
 })
